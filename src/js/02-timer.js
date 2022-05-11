9ccdbfef0b4c8299a639a9a9b9405a87246d581b
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-05-11 23:30:27 +0300

    upd2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,13 +3,49 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 console.log(typeof flatpickr);
 
+// =======DOM=============
+
 const refs = {
   input: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
-console.log(refs.input);
-console.log(refs.startBtn);
+function showsRefs() {
+  console.log(refs.input);
+  console.log(refs.startBtn);
+  console.log(refs.days.value);
+  console.log(refs.hours);
+  console.log(refs.minutes);
+  console.log(refs.seconds);
+}
+showsRefs();
+
+// =======Function convertMs=============
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+//==========CREATE NEW INSTANCE===========
 
 const options = {
   enableTime: true,
@@ -21,22 +57,44 @@ const options = {
   },
 };
 
+let ms = null;
+
 const ff = flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+  onClose(selectedDates) {},
 });
 
-const getTime = ff.config.defaultDate.getTime();
-console.log(getTime);
-// console.log(typeof flatpickr);
+const onClose = selectedDates => {
+  // console.log(selectedDates[0].getTime());
+  if (selectedDates[0] < new Date()) {
+    alert('Please choose a date in the future');
+    refs.startBtn.setAttribute('disabled');
+    return;
+  }
+  ms = selectedDates[0] - new Date();
+  const getConvertMs = convertMs(ms);
+  showConvertMsData(getConvertMs);
+};
+// console.log(flatpickr.config.onClose());
+// console.log(typeof gg);
+
+const showConvertMsData = getConvertMs => {
+  refs.days.textContent = getConvertMs.days;
+  refs.hours.textContent = getConvertMs.hours;
+  refs.minutes.textContent = getConvertMs.minutes;
+  refs.seconds.textContent = getConvertMs.seconds;
+};
+
+console.log(ms);
 
 const handleStartBtnClick = () => {
-  //   console.log(refs.input.flatpickr());
+  console.log('Hello');
+  setInterval(() => {
+    onClose(ff.selectedDates);
+  }, 1000);
 };
 
 refs.startBtn.addEventListener('click', handleStartBtnClick);
